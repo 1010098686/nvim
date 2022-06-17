d8bb9a110395297ecbe5151e2cf27913e70ca00c
房康 <fk1010098686@outlook.com>
2022-06-17 14:44:11 +0800

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,4 +30,6 @@ return require('packer').startup(function()
     use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
