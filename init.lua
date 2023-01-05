231e6cbbade25a1145300586314f3d359ff5676a
房康 <fk1010098686@outlook.com>
2023-01-05 17:42:59 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+require('basic')
+require('plugins')
+
+
+require('plugin-config/nvim-tree')
+require('plugin-config/bufferline')
+require('plugin-config/comment')
+require('plugin-config/nvim-autopair')
+require('plugin-config/galaxyline')
+require('plugin-config/indent-guides')
+require('plugin-config/telescope')
+require('plugin-config/FTerm')
+require('plugin-config/lsp-zero')
